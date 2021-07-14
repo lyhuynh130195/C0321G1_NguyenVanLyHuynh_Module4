28b036e7cbfcb2b2e8d1227f6ad1fb8b200b0bab
ly-huynh <lyhuynh130195@gmail.com>
2021-07-14 18:44:40 +0700

    commit

--- 1.overview_spring_mvc/exercise/dictionary/src/main/java/com/dictionary/repository/DictionaryRepo.java
+++ 1.overview_spring_mvc/exercise/dictionary/src/main/java/com/dictionary/repository/DictionaryRepo.java
@@ -1,5 +1,8 @@
 package com.dictionary.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface DictionaryRepo {
     String findByEnglish(String en);
 
--- 1.overview_spring_mvc/exercise/dictionary/src/main/java/com/dictionary/repository/DictionaryRepoImpl.java
+++ 1.overview_spring_mvc/exercise/dictionary/src/main/java/com/dictionary/repository/DictionaryRepoImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Repository
 public class DictionaryRepoImpl implements DictionaryRepo {
     private static List<Dictionary> dictionaryList = new ArrayList<>();
 
